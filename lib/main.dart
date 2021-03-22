4b61ca8babaec58c87a9e41fca99c7efb37baed7
Olga Pelipets <pelipets.olga.developer@gmail.com>
2021-03-22 17:50:40 +0200

    Localisation start

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,31 @@
 import 'package:coleman/navigation/navigation.dart';
 import 'package:coleman/ui/common/constants.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'injection.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   configureDependencies();
-  runApp(MyApp());
+
+  const Locale _enLocale = Locale('en', 'US');
+  const Locale _ruLocale = Locale('ru', 'RU');
+
+  runApp(EasyLocalization(
+      supportedLocales: const [_enLocale, _ruLocale],
+      path: 'assets/translations',
+      fallbackLocale: _enLocale,
+      child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       title: 'Coleman',
       theme: UIConstants.materialTheme,
       routes: AppNavigation.getNavigationRoutes(),
